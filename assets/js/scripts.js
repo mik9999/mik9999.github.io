f26ea53f4d27a22dc9f0c6f3d37637841aba4d84
mik9999 <mik9999@users.noreply.github.com>
2014-10-24 12:15:20 +0200

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -73,7 +73,7 @@
 		jQuery("#slider").superslides({
 			animation: "fade", 		// slide|fade
 			pagination: true, 		// true|false
-			play: true,	 		// false to disable autoplay -OR- miliseconds (eg.: 1000 = 1s)
+			play: 3000,	 		// false to disable autoplay -OR- miliseconds (eg.: 1000 = 1s)
 			animation_speed: 600,	// animation transition
 
 			elements: {
