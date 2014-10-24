e273364423c3b91d381586e493139a26f2c3add9
mik9999 <mik9999@users.noreply.github.com>
2014-10-24 14:47:43 +0200

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -923,6 +923,6 @@ jQuery("#newsletter-subscribe").bind("click", function(e) {
 		jQuery(this).siblings().removeClass('faded');
 	});
 
-$('fullscreen-img').waitForImages(function() {
+$('.fullscreen-img').waitForImages(function() {
     $(this).fadeIn();
 });
